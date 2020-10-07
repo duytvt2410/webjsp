532d1b86c9269b9dbc70194c704d211bbb37b0cd
duytvt2410 <48060188+duytvt2410@users.noreply.github.com>
2020-10-07 17:00:21 +0700

    update

--- src/main/java/dao/imp/UserDAO.java
+++ src/main/java/dao/imp/UserDAO.java
@@ -23,7 +23,7 @@ private static UserDAO dao = null;
 	public boolean insert(UserModel model) {
 		StringBuilder sql = new StringBuilder();
 		sql.append(
-				"INSERT INTO user (id, fullname, email, password, role, status, created_date, created_by, updated_date, updated_by)");
+				"INSERT INTO account (id, fullname, email, password, role, status, created_date, created_by, updated_date, updated_by)");
 		sql.append(" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		return excute(sql.toString(), model.getId(), model.getFullName(), model.getEmail(), model.getPassword(), model.getRole(),
 				model.getStatus(), model.getCreateDate(), model.getCreateBy(), model.getUpdateDate(),
@@ -39,7 +39,7 @@ private static UserDAO dao = null;
 	@Override
 	public UserModel findOneByEmail(String email) {
 		StringBuilder sql = new StringBuilder();
-		sql.append("SELECT * FROM user");
+		sql.append("SELECT * FROM account");
 		sql.append(" WHERE email = ?");
 		List<UserModel> list = query(sql.toString(), new UserMapper(), email);
 		return list.size() != 0 ? list.get(0) : null;
@@ -48,7 +48,7 @@ private static UserDAO dao = null;
 	@Override
 	public UserModel findOneByEmailAndPassword(String email, String password) {
 		StringBuilder sql = new StringBuilder();
-		sql.append("SELECT * FROM user");
+		sql.append("SELECT * FROM account");
 		sql.append(" WHERE email = ? AND password = ?");
 		List<UserModel> list = query(sql.toString(), new UserMapper(), email, password);
 		return list.size() != 0 ? list.get(0) : null;
